bd88e341e808120ab547f02cc8875861b88a4d07
shashi1920 <shashikant.sharma.cse12@iitbhu.ac.in>
2016-07-08 04:56:34 +0530

    rm

--- temo.py
+++
@@ -1,4 +0,0 @@
-import requests
-response = requests.head('http://hindimasti.net/bollywood/files/New%20Latest%202016/Kapoor%20&%20Sons/Buddhu%20Sa%20Mann.mp3')
-
-print response.headers['content-type']
